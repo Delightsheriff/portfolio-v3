63679f6a00d589ce2df50ec780fb9d766cff25c7
Delight Sheriff <delightsheriff@gmail.com>
2025-06-19 14:38:30 +0100

    added resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,11 @@
+import { ResumePage } from "@/components/resume-page";
+
+export const metadata = {
+  title: "Resume - Amadi-Sheriff Delight",
+  description:
+    "Download my resume or view my professional experience and qualifications.",
+};
+
+export default function Resume() {
+  return <ResumePage />;
+}
