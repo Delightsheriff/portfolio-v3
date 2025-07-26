919b13fe995f041f68323b82b8431d53d4c16ee2
delightsheriff <delightsheriff@gmail.com>
2025-07-26 19:55:23 +0100

    added video-pitch component

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,9 +4,6 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemaTypes";
 import { EnvVariables } from "./lib/env";
 
-console.log("Sanity Project ID:", EnvVariables.SANITY_PROJECT_ID);
-console.log("Sanity Dataset:", EnvVariables.SANITY_DATASET);
-
 export default defineConfig({
   name: "default",
   title: "portfolio-v3",
