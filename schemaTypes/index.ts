971fc46d0179b716ded192ce7cef13c6c1ae369d
delightsheriff <delightsheriff@gmail.com>
2025-07-26 19:37:23 +0100

    added video pitch sanity schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,13 @@ import experience from "./experience";
 import hero from "./hero";
 import projects from "./project";
 import resume from "./resume";
+import videoPitch from "./video-pitch";
 
-export const schemaTypes = [projects, hero, experience, about, resume];
+export const schemaTypes = [
+  projects,
+  hero,
+  experience,
+  about,
+  resume,
+  videoPitch,
+];
