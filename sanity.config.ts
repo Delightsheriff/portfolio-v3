e5d4383b5b46a28e5d89c380a2c77b0392ca6c1f
Delight Sheriff <delightsheriff@gmail.com>
2026-04-16 20:01:00 +0100

    fixed npm errors

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemaTypes";
 import { EnvVariables } from "./lib/env";
-import { codeInput } from "@sanity/code-input";
 
 export default defineConfig({
   name: "default",
@@ -13,7 +10,7 @@ export default defineConfig({
   dataset: EnvVariables.SANITY_DATASET,
   basePath: "/studio",
 
-  plugins: [structureTool(), visionTool(), codeInput()],
+  plugins: [],
 
   schema: {
     types: schemaTypes,
